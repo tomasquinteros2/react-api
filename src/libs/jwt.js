7111dc3ee3e24637dea2eee9dc3a55e3f38edd17
ticope <82914165+ticope@users.noreply.github.com>
2024-07-03 12:13:08 -0300

    prueba de token

--- src/libs/jwt.js
+++ src/libs/jwt.js
@@ -2,11 +2,7 @@ const jwt = require("jsonwebtoken");
 
 function createAccessToken(payload) {
     return new Promise((resolve, reject) => {
-        jwt.sign({
-            payload,
-        }, 'secret123', {
-            expiresIn: "1d",
-        }, (err, token) => {
+        jwt.sign(payload, 'secret123', {expiresIn: '1h'}, (err, token) => {
             if (err) {
                 console.error("Error creating token:", err);
                 reject(err);
