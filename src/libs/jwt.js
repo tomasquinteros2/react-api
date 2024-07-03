59c021af7104ce1a28a3bd1c560e62321d3990c0
ticope <82914165+ticope@users.noreply.github.com>
2024-07-03 12:16:22 -0300

    prueba token expires

--- src/libs/jwt.js
+++ src/libs/jwt.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 
 function createAccessToken(payload) {
     return new Promise((resolve, reject) => {
-        jwt.sign(payload, 'secret123', {expiresIn: '1h'}, (err, token) => {
+        jwt.sign(payload, 'secret123', {expiresIn: 80000}, (err, token) => {
             if (err) {
                 console.error("Error creating token:", err);
                 reject(err);
